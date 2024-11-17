9adc4863b57c76aa58a771b441dbae30b6538c8e
aharist <aharist03@gmail.com>
2024-11-17 10:13:17 +0700

    membuat project taskly

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import "dotenv/config";
+import userRouter from "./routes/user.route.js";
+import { errorHandler } from './configs/midleware.js';
+
+const app = express();//library express initialized
+const PORT = process.env.PORT;
+
+//json middleware
+app.use(express.json());
+
+//api
+app.use("/api/v1/users", userRouter);
+
+
+app.use("*",(req, res)=>{
+    res.status(404).json({
+        message: "not found",
+    });
+});
+
+app.use(errorHandler);
+
+//start the server
+app.listen(PORT, () => {
+    console.log(`Server started, listening on port ${PORT}`);
+})
