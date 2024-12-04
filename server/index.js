bf9d8c34d5077854d2b70a270f5a3582682f7cca
aharist <aharist03@gmail.com>
2024-12-04 21:48:40 +0700

    pembersihan spasi berlebih

--- server/index.js
+++ server/index.js
@@ -9,7 +9,6 @@ import cors from 'cors';
 const app = express();//library express initialized
 const PORT = process.env.PORT;
 
-//json middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(
@@ -19,27 +18,20 @@ app.use(
     })
 );
 
-//api
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/auth", authRouter);
-
 
 app.use("/", (req, res) => {
     res.status(200).json({
         message: "Welcome",
     });
 });
-
-
 app.use("*", (req, res) => {
     res.status(404).json({
         message: "not found",
     });
 });
-
 app.use(errorHandler);
-
-//start the server
 app.listen(PORT, () => {
     console.log(`Server started, listening on port ${PORT}`);
 })
